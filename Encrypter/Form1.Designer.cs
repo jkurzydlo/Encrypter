d2ecc23099bd9880f0b62d1717254e199fdcfe88
Jacek <jacek.kurzydlo@protonmail.ch>
2021-07-20 13:24:19 +0200

    aes, efs encryption available

---
+++ Encrypter/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Encrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 262);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Drop files here";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(219, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 35);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(415, 344);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(190, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Clear list";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 47);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(56, 21);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "AES";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 74);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(125, 21);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "EFS encryption";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(595, 76);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(182, 262);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Encryption algorithm";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 402);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Encrypter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Encrypter/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Encrypter
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        enum ENCRYPTION_TYPE
+        {
+            AES,EFS
+        }
+
+        ENCRYPTION_TYPE encryption;
+        string[] files = new string[] { };
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(dragEnter);
+            this.DragDrop += new DragEventHandler(dragDrop);
+
+        }
+
+        void dragEnter(object sender, DragEventArgs args)
+        {
+            if (args.Data.GetDataPresent(DataFormats.FileDrop))
+                args.Effect = DragDropEffects.Copy;
+        }
+
+        void dragDrop(object sender, DragEventArgs args)
+        {
+           // bool counter;
+           // args.Data.
+           files  = (string[])args.Data.GetData(DataFormats.FileDrop);
+            foreach (var file in files)
+            {
+               // if (label1.Text.Contains("Empty")) label1.Text = "";
+                label1.Text += Path.GetFullPath(file) + "\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(var file in files)
+            {
+               if((File.GetAttributes(Path.GetFullPath(file)) & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    foreach (var path in Directory.EnumerateFiles(Path.GetFullPath(file), "*.*", SearchOption.AllDirectories))
+                    {
+                        switch (encryption)
+                        {
+                            case ENCRYPTION_TYPE.EFS:File.Encrypt(path); break;
+                            case ENCRYPTION_TYPE.AES:
+                                {
+                                    var file_content = System.IO.File.ReadAllText(path);
+                                    EncryptAesManaged(file_content);
+
+                                    break;
+                                }
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    switch (encryption)
+                    {
+                        case ENCRYPTION_TYPE.EFS: File.Encrypt(Path.GetFullPath(file)); break;
+                        case ENCRYPTION_TYPE.AES:
+                            {
+                                var file_content = System.IO.File.ReadAllText(Path.GetFullPath(file));
+                                EncryptAesManaged(file_content);
+                                break;
+                            }
+                    }
+                }
+    
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var file in files)
+            {
+                if ((File.GetAttributes(Path.GetFullPath(file)) & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    foreach (var path in Directory.EnumerateFiles(Path.GetFullPath(file), "*.*", SearchOption.AllDirectories))
+                        File.Decrypt(path);
+                }
+                else
+                {
+
+                    File.Decrypt(Path.GetFullPath(file));
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Array.Clear(files, 0, files.Length);
+            label1.Text = "";
+           // label1.auto
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+          //  if (comboBox1.Text == "")
+          //  {
+                //comboBox1.Text = "Select";
+          //      button1.Enabled = false;
+           // }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string defText = "Select encryption algorithm";
+           // if (comboBox1.SelectedText != defText ) comboBox1.Items.Remove(defText);
+        }
+
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+        static void EncryptAesManaged(string raw)
+        {
+            try
+            {
+                // Create Aes that generates a new key and initialization vector (IV).    
+                // Same key must be used in encryption and decryption    
+                using (AesManaged aes = new AesManaged())
+                {
+                    // Encrypt string    
+                    byte[] encrypted = Encrypt(raw, aes.Key, aes.IV);
+                    // Print encrypted string    
+                    Console.WriteLine($"Encrypted data: {System.Text.Encoding.UTF8.GetString(encrypted)}");
+                    // Decrypt the bytes to a string.    
+                    string decrypted = Decrypt(encrypted, aes.Key, aes.IV);
+                    // Print decrypted string. It should be same as raw data    
+                    Console.WriteLine($"Decrypted data: {decrypted}");
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+           // Console.ReadKey();
+        }
+        static byte[] Encrypt(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+            // Create a new AesManaged.    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create encryptor    
+                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+                // Create MemoryStream    
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    // Create crypto stream using the CryptoStream class. This class is the key to encryption    
+                    // and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream    
+                    // to encrypt    
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        // Create StreamWriter and write data to a stream    
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(plainText);
+                        encrypted = ms.ToArray();
+                    }
+                }
+            }
+            // Return encrypted data    
+            return encrypted;
+        }
+        static string Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+            // Create AesManaged    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create a decryptor    
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+                // Create the streams used for decryption.    
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    // Create crypto stream    
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        // Read crypto stream    
+                        using (StreamReader reader = new StreamReader(cs))
+                            plaintext = reader.ReadToEnd();
+                    }
+                }
+            }
+            return plaintext;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) encryption = ENCRYPTION_TYPE.AES;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked) encryption = ENCRYPTION_TYPE.EFS;
+        }
+    }
+}
